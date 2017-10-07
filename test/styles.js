89b0813ba2ad80fc28830da7af2618b8ea85e183
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2017-10-07 16:26:08 +0200

    Tests: Refactoring in styles.

--- test/styles.js
+++ test/styles.js
@@ -7,11 +7,12 @@
 const styles = require( '../lib/styles.js' )();
 
 describe( 'styles', () => {
-	it( 'work', () => {
-		for ( let name in styles ) {
-			console.log( `---${ styles[ name ]( name ) }---` );
-			process.stdout.write( styles[ name ]( name ) );
-			process.stdout.write( '\n' );
+	it( 'are defined as functions', () => {
+		expect( styles ).to.be.an.object;
+		expect( Object.keys( styles ) ).to.have.length( 37 );
+
+		for ( const name in styles ) {
+			expect( styles[ name ] ).to.be.a.function;
 		}
 	} );
 } );
